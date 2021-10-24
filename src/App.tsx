236477c9dd43f442fa9df3c4f4c3ce8bdd24c2e8
Steve Tointon <steventointon@aol.com>
2021-10-24 06:40:55 +0100

    added meta mask connectivity
    Added meta mask button on right of the navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import logo from './logo.png';
+import logo from './WTYFull.png';
 import * as React from "react";
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
@@ -13,6 +13,12 @@ import LeaguePage from './components/LeaguePage';
 import './App.css';
 import Web3 from 'web3';
 import { WTY_ADDRESS } from './components/LeaguePage';
+import MetaMaskOnboarding from "@metamask/onboarding";
+import { textAlign } from '@mui/system';
+
+const ONBOARD_TEXT = "Click here to install MetaMask!";
+const CONNECT_TEXT = "Connect";
+const CONNECTED_TEXT = "Connected";
 
 const ERC20 = require("./abis/ERC20.json");
 
@@ -24,7 +30,10 @@ declare global {
 
 
 function App() {
+  const [buttonText, setButtonText] = React.useState(ONBOARD_TEXT);
+  const [isDisabled, setDisabled] = React.useState(false);
   const [accounts, setAccounts] = React.useState([]);
+  const onboarding = React.useRef<MetaMaskOnboarding>();
 
   async function setApproval() {
     const web3 = new Web3(window.ethereum);
@@ -32,6 +41,56 @@ function App() {
 
     await contractERC20.methods.approve(WTY_ADDRESS, "1000000000000000000000000000000000000").send({ from: accounts[0] });
   }
+
+  React.useEffect(() => {
+    if (!onboarding.current) {
+      onboarding.current = new MetaMaskOnboarding();
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      if (accounts.length > 0) {
+        setButtonText(CONNECTED_TEXT);
+        setDisabled(true);
+        if (onboarding.current) {
+          onboarding.current.stopOnboarding();
+        }
+      } else {
+        setButtonText(CONNECT_TEXT);
+        setDisabled(false);
+      }
+    }
+  }, [accounts]);
+
+  React.useEffect(() => {
+    function handleNewAccounts(newAccounts: React.SetStateAction<never[]>) {
+      setAccounts(newAccounts);
+    }
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then(handleNewAccounts);
+      window.ethereum.on("accountsChanged", handleNewAccounts);
+      return () => {
+        window.ethereum.off("accountsChanged", handleNewAccounts);
+      };
+    }
+  }, []);
+
+  const onClick = () => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((newAccounts: React.SetStateAction<never[]>) =>
+          setAccounts(newAccounts)
+        );
+    } else {
+      if (onboarding.current) {
+        onboarding.current.startOnboarding();
+      }
+    }
+  };
 
   React.useEffect(() => {
     function handleNewAccounts(newAccounts: React.SetStateAction<never[]>) {
@@ -47,17 +106,17 @@ function App() {
   }, []);
 
   return (
+
+
+    
     <Router>
-      <div className="App">
+      <div>
         <nav>
-          <Grid container spacing={2}>
-            <Grid item>
               <Link to="/"><img src={logo} alt={'logo'} height={100} /></Link>
-            </Grid>
-            <Grid item>
               <Link to="/leagues"><Button style={{ fontSize: 20 }}>Leagues</Button></Link>
-            </Grid>
-          </Grid>
+              <Button variant='contained' onClick={() => setApproval()}>Set DAI approval</Button>
+              <Button disabled={isDisabled} onClick={onClick}>{buttonText}</Button> 
+
         </nav>
         <Switch>
           <Route path="/leagues">
@@ -67,8 +126,7 @@ function App() {
             <LeaguePage />
           </Route>
           <Route path="/">
-            <b>WELCOME TO WTY </b>
-            <Button variant='contained' onClick={() => setApproval()}>Set DAI approval</Button>
+            <header>Welcome to WTY, connect to your MetaMask wallet to interact with our site. Once connected, must give us approval to depost DAI</header>
           </Route>
         </Switch>
       </div >
