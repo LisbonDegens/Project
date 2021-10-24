b78f28f3a9f09ec3ecbb4ac8293132dd47ef1561
fraserdscott <fraserdscott@gmail.com>
2021-10-24 04:25:33 +0100

    Add users current staked

--- src/components/LeaguePage.tsx
+++ src/components/LeaguePage.tsx
@@ -5,20 +5,19 @@ import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { LEAGUES } from "./Leagues";
+import BigNumber from 'bignumber.js';
 
 const WTY = require("../abis/WTY.json");
 const ERC20 = require("../abis/ERC20.json");
 
 declare let window: any;
 
-const WTY_ADDRESS = "0xa0348368A2732650A15324f29e69D71EB7737bf5";
-const ATOKEN = "0x27F8D03b3a2196956ED754baDc28D73be8830A6e";
-
-
+const WTY_ADDRESS = "0x2A4588F1B477F17CCF4dCD71327aA17FaB24526D";
+const ATOKEN_ADDRESS = "0x27F8D03b3a2196956ED754baDc28D73be8830A6e";
 
 async function calculatePrize(leagueIndex: number) {
     const web3 = new Web3(window.ethereum);
-    const contractERC20 = new web3.eth.Contract(ERC20, ATOKEN);
+    const contractERC20 = new web3.eth.Contract(ERC20, ATOKEN_ADDRESS);
     const contractWTY = new web3.eth.Contract(WTY, WTY_ADDRESS);
 
     const balance = await contractERC20.methods.balanceOf(WTY_ADDRESS).call();
@@ -27,15 +26,21 @@ async function calculatePrize(leagueIndex: number) {
     return balance - totalStake;
 }
 
+async function getUserStake(leagueIndex: number, user: string) {
+    const web3 = new Web3(window.ethereum);
+    const contractWTY = new web3.eth.Contract(WTY, WTY_ADDRESS);
 
+    return await contractWTY.methods.getUserStake(leagueIndex, user).call();
+}
 
 const DECIMALS = 18;
 
 export default function LeaguePage() {
     let { league }: { league: string } = useParams();
     var [accounts, setAccounts] = React.useState([]);
+    var [userStake, setUserStake] = useState(0);
 
-    var [amount, setAmount] = useState(0);
+    var [amount, setAmount] = useState(new BigNumber(0));
     var [prize, setPrize] = useState(0);
 
     React.useEffect(() => {
@@ -51,6 +56,14 @@ export default function LeaguePage() {
         };
     }, []);
 
+    React.useEffect(() => {
+        async function handleUserStake() {
+            setUserStake(await getUserStake(LEAGUES[league].leagueIndex, accounts[0]));
+        }
+        if (accounts.length > 0)
+            handleUserStake();
+    }, [accounts, league]);
+
     async function setApproval() {
         const web3 = new Web3(window.ethereum);
         const contractERC20 = new web3.eth.Contract(ERC20, "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063");
@@ -65,7 +78,7 @@ export default function LeaguePage() {
         contractWTY.methods.withdraw(leagueIndex).send({ from: accounts[0] });
     }
 
-    async function deposit(leagueIndex: number, amount: number) {
+    async function deposit(leagueIndex: number, amount: BigNumber) {
         const web3 = new Web3(window.ethereum);
         const contract = new web3.eth.Contract(WTY, WTY_ADDRESS);
 
@@ -99,7 +112,7 @@ export default function LeaguePage() {
                 <Grid item>
                     <TextField
                         label="Amount"
-                        onChange={(e: any) => setAmount(e.target.value)}
+                        onChange={(e: any) => setAmount(new BigNumber(e.target.value * (10 ** DECIMALS)))}
                     />
                     <img height={40} alt={'DAI'} src="https://s2.coinmarketcap.com/static/img/coins/200x200/4943.png" />
                 </Grid>
@@ -109,7 +122,7 @@ export default function LeaguePage() {
             </Grid>
             <Grid container direction='column' style={{ borderStyle: 'dotted', marginTop: 20 }}>
                 <Grid item>
-                    <p>You currently have FIXME DAI staked.</p>
+                    <p>You currently have {userStake / (10 ** DECIMALS)} DAI staked.</p>
                 </Grid>
                 <Grid item>
                     <Button variant='contained' onClick={() => withdraw(LEAGUES[league].leagueIndex)}>Withdraw</Button>
